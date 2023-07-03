8cb95396d4c5d7e46d23f8800b0349ceef76b71b
ishige <hiroyukiishige@hotmail.com>
2023-07-04 07:29:56 +0900

    Update

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50) #Create text
 
-test_surface = pygame.image.load("graphics/sky.png")
+text_surface = test_font.render("My game", True, "white")
+sky_surface = pygame.image.load("graphics/sky(800).jpg") #Import image
+ground_surface = pygame.image.load("graphics/ground(800).jpg") #Import image
+snail_surface = pygame.image.load("graphics/snail_2.png") #Import image
+snail_x_pos = 750
 
 while True:  # This while roop is important to keep screen showing
     for event in pygame.event.get():  # Keep looking all event
@@ -15,7 +20,12 @@ while True:  # This while roop is important to keep screen showing
             exit()  # stop system and exit while roop
 
     # draw all out elements
-    screen.blit(test_surface, (30, 30))  # connect test_surfact to screen
+    screen.blit(text_surface, (0, 0))  # attach image to screen
+    screen.blit(sky_surface, (30, 30))  # attach image to screen
+    screen.blit(ground_surface, (30, 300))  # attach image to screen
+    snail_x_pos -=1
+    screen.blit(snail_surface, (snail_x_pos, 250))  # attach image to screen
+
 
     pygame.display.update()  # update everything
     clock.tick(60)  # set game to refreash 60 times/second at MAX
--- reference.py
+++ reference.py
@@ -1,7 +1,7 @@
 """
 Ultimate introduction of pygame
 
-TODO see from 2:00
+TODO see from 34:00
 https://youtu.be/AY9MnQ4x3zk
 
 
