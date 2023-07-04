95cfbb526607263903ec898b501063af14d84581
ishige <hiroyukiishige@hotmail.com>
2023-07-05 06:44:01 +0900

    Update

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
-test_font = pygame.font.Font("font/Pixeltype.ttf", 50) #Create text
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)  # Create text
 
 text_surface = test_font.render("My game", True, "white")
-sky_surface = pygame.image.load("graphics/sky(800).jpg") #Import image
-ground_surface = pygame.image.load("graphics/ground(800).jpg") #Import image
-snail_surface = pygame.image.load("graphics/snail_2.png") #Import image
+sky_surface = pygame.image.load("graphics/sky(800).jpg")  # Import image
+ground_surface = pygame.image.load("graphics/ground(800).jpg")  # Import image
+snail_surface = pygame.image.load("graphics/snail_2.png")  # Import image
 snail_x_pos = 750
 
 while True:  # This while roop is important to keep screen showing
@@ -23,9 +23,11 @@ while True:  # This while roop is important to keep screen showing
     screen.blit(text_surface, (0, 0))  # attach image to screen
     screen.blit(sky_surface, (30, 30))  # attach image to screen
     screen.blit(ground_surface, (30, 300))  # attach image to screen
-    snail_x_pos -=1
     screen.blit(snail_surface, (snail_x_pos, 250))  # attach image to screen
+    snail_x_pos -= 1
 
+    if snail_x_pos < 0:
+        snail_x_pos = 750
 
     pygame.display.update()  # update everything
     clock.tick(60)  # set game to refreash 60 times/second at MAX
