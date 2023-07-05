9c546d0fccf5a925f32959b82383fa941e4f7fcb
ishige <hiroyukiishige@hotmail.com>
2023-07-06 07:00:50 +0900

    Update

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
-test_font = pygame.font.Font("font/Pixeltype.ttf", 50)  # Create text
 
+FLAME_RATE = 60  # set refreash times/second
+
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)  # Create text
 text_surface = test_font.render("My game", True, "white")
-sky_surface = pygame.image.load("graphics/sky(800).jpg")  # Import image
-ground_surface = pygame.image.load("graphics/ground(800).jpg")  # Import image
-snail_surface = pygame.image.load("graphics/snail_2.png")  # Import image
-snail_x_pos = 750
+
+"""
+Import image 
+"convert()", and "convert_alpha()" make graphics easier and faster to be handled by pygame
+"""
+sky_surface = pygame.image.load("graphics/sky(800).jpg").convert()  # Import image
+ground_surface = pygame.image.load("graphics/ground(800).jpg").convert()  # Import image
+snail_surface = pygame.image.load("graphics/snail/snail_2.png").convert_alpha()
+snail_rect = snail_surface.get_rect(bottomright=(800, 300))
+
+# snail_x_pos = 750
+
+player_surface = pygame.image.load("graphics/player/player.png").convert_alpha()  # import image
+player_rect = player_surface.get_rect(bottomleft=(50, 300))  # set rectanble
 
 while True:  # This while roop is important to keep screen showing
     for event in pygame.event.get():  # Keep looking all event
@@ -21,13 +33,18 @@ while True:  # This while roop is important to keep screen showing
 
     # draw all out elements
     screen.blit(text_surface, (0, 0))  # attach image to screen
-    screen.blit(sky_surface, (30, 30))  # attach image to screen
-    screen.blit(ground_surface, (30, 300))  # attach image to screen
-    screen.blit(snail_surface, (snail_x_pos, 250))  # attach image to screen
-    snail_x_pos -= 1
+    screen.blit(sky_surface, (50, 50))  # attach image to screen
+    screen.blit(ground_surface, (50, 300))  # attach image to screen
+    screen.blit(snail_surface, snail_rect)  # attach image to screen
+    screen.blit(player_surface, player_rect)  # attach image to screen
 
-    if snail_x_pos < 0:
-        snail_x_pos = 750
+    # snail_x_pos -= 2
+    snail_rect.left -=1
+    player_rect.left +=1
+
+
+    if snail_rect.left < 0:
+        snail_rect.left = 800
 
     pygame.display.update()  # update everything
-    clock.tick(60)  # set game to refreash 60 times/second at MAX
+    clock.tick(FLAME_RATE)
--- reference.py
+++ reference.py
@@ -1,8 +1,10 @@
 """
 Ultimate introduction of pygame
 
-TODO see from 45:00
+#TODO see from 1:00:00
 https://youtu.be/AY9MnQ4x3zk
+
+Explanation on Rectangle 51:40
 
 
 """
