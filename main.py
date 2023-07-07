bec09ccb4e5b61b08f55f559a9691b4283ea3db7
ishige <hiroyukiishige@hotmail.com>
2023-07-08 07:38:59 +0900

    Update

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ clock = pygame.time.Clock()
 FLAME_RATE = 60  # set refreash times/second
 
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)  # Create text
-text_surface = test_font.render("My game", True, "white")
-
+text_surf = test_font.render("My game", True, "#ffffff")
+score_surf = test_font.render("Score", True, "white")
+score_rect = score_surf.get_rect(midbottom=(400, 50))
 """
 Import image 
 "convert()", and "convert_alpha()" make graphics easier and faster to be handled by pygame
@@ -23,7 +24,7 @@ snail_rect = snail_surf.get_rect(bottomright=(800, 300))
 # snail_x_pos = 750
 
 player_surf = pygame.image.load("graphics/player/player.png").convert_alpha()  # import image
-player_rect = player_surf.get_rect(bottomleft=(50, 300))  # set rectanble
+player_rect = player_surf.get_rect(bottomleft=(50, 300))  # set rectangle
 
 while True:  # This while roop is important to keep screen showing
     for event in pygame.event.get():  # Keep looking all event
@@ -35,27 +36,37 @@ while True:  # This while roop is important to keep screen showing
         #     if player_rect.collidepoint(event.pos):
         #         print("collision")
 
-
     # draw all out elements
-    screen.blit(text_surface, (0, 0))  # attach image to screen
+    screen.blit(text_surf, (0, 0))  # attach image to screen
     screen.blit(sky_surface, (50, 50))  # attach image to screen
     screen.blit(ground_surface, (50, 300))  # attach image to screen
     screen.blit(snail_surf, snail_rect)  # attach image to screen
     screen.blit(player_surf, player_rect)  # attach image to screen
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+
+    # pygame.draw.line(screen, "white", start_pos=(50, 50), end_pos=pygame.mouse.get_pos(), width=5) #Draw line
+    screen.blit(score_surf, score_rect)
 
     # print(snail_rect.left)
-    snail_rect.x -=3
-    player_rect.x +=1
+    snail_rect.x -= 3
+    player_rect.x += 1
 
     if snail_rect.left < 50: snail_rect.left = 800
 
     # if player_rect.colliderect(snail_rect): #This will return 0(False) if no collide, 1 if collide (True)
     #     print("collision")
 
-    mouse_pos = pygame.mouse.get_pos() #Get position of the mouse
+#TODO to set jump when space key is pressed
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        print("jump")
+
+    # mouse position ------------------------------
+    mouse_pos = pygame.mouse.get_pos()  # Get position of the mouse
     if player_rect.collidepoint(mouse_pos):
         print("collision")
-        print(pygame.mouse.get_pressed()) #Check if any button of mouse if pressed
+        print(pygame.mouse.get_pressed())  # Check if any button of mouse if pressed
+    #-----------------------------------------------
 
     pygame.display.update()  # update everything
     clock.tick(FLAME_RATE)
--- reference.py
+++ reference.py
@@ -5,6 +5,10 @@ Ultimate introduction of pygame
 https://youtu.be/AY9MnQ4x3zk
 
 Explanation on Rectangle 51:40
+Explanation on Keyboard input 1:30:40
 
+"""
 
+"""
+Original documentation of Pygame
 """
